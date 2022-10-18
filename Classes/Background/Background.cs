72cc0997330e4a98a1d0024db0637bf0366f178c
Nidrux <originalfroaz@gmail.com>
2022-10-18 20:26:52 +0200

    Refactored to dyanmic background layer loading

--- Classes/Background/Background.cs
+++ Classes/Background/Background.cs
@@ -4,50 +4,27 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Forest_of_wrath.Classes.Background
 {
     internal class Background: IGameObject
     {
-
-        /* 
-            Refactor -> Read trough Content/Background/ -> Load in all files trough Content.Load().
-            Push them to an array? Read them out to draw them?
-        */
-        Texture2D _backgroundTexture_0;
-        Texture2D _backgroundTexture_1;
-        Texture2D _backgroundTexture_2;
-        Texture2D _backgroundTexture_3;
-        Texture2D _backgroundTexture_4;
-        Texture2D _backgroundTexture_5;
-        Texture2D _backgroundTexture_6;
-        Texture2D _backgroundTexture_7;
-        Texture2D _backgroundTexture_8;
-        Texture2D _backgroundTexture_9;
-        Texture2D _backgroundTexture_10;
-        Texture2D _backgroundTexture_11;
-
-        private int offsetY;
-
+        List<Layer> textures = new List<Layer>();
         public Background(ContentManager content)
         {
-            _backgroundTexture_0 = content.Load<Texture2D>("Background/layer_0");
-            _backgroundTexture_1 = content.Load<Texture2D>("Background/layer_1");
-            _backgroundTexture_2 = content.Load<Texture2D>("Background/layer_2");
-            _backgroundTexture_3 = content.Load<Texture2D>("Background/layer_3");
-            _backgroundTexture_4 = content.Load<Texture2D>("Background/layer_4");
-            _backgroundTexture_5 = content.Load<Texture2D>("Background/layer_5");
-            _backgroundTexture_6 = content.Load<Texture2D>("Background/layer_6");
-            _backgroundTexture_7 = content.Load<Texture2D>("Background/layer_7");
-            _backgroundTexture_8 = content.Load<Texture2D>("Background/layer_8");
-            _backgroundTexture_9 = content.Load<Texture2D>("Background/layer_9");
-            _backgroundTexture_10 = content.Load<Texture2D>("Background/layer_10");
-            _backgroundTexture_11 = content.Load<Texture2D>("Background/layer_11");
-
-            offsetY = 680 - 793 - 100;
+            string[] filesList = Directory.GetFiles("./Content/Background");
+            foreach (string file in filesList)
+            {
+                string path = Path.GetFileNameWithoutExtension(file);
+                Layer layer = new Layer(path, 0, content);
+                textures.Add(layer);
+            }
         }
         public void Update()
         {
@@ -55,18 +32,11 @@ namespace Forest_of_wrath.Classes.Background
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_backgroundTexture_11, new Vector2(0, offsetY), Color.White) ;
-            spriteBatch.Draw(_backgroundTexture_10, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_9, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_8, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_7, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_6, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_5, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_4, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_3, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_2, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_1, new Vector2(0,offsetY), Color.White);
-            spriteBatch.Draw(_backgroundTexture_0, new Vector2(0,offsetY), Color.White);
+            textures.Sort((x, y) => y.getLayerPos().CompareTo(x.getLayerPos()));
+            foreach (Layer texture in textures)
+            {
+                texture.Draw(spriteBatch);
+            }
         }
     }
 }
---
+++ Classes/Background/Layer.cs
@@ -0,0 +1,40 @@
+﻿using Forest_of_wrath.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.MediaFoundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest_of_wrath.Classes.Background
+{
+    internal class Layer : IGameObject
+    {
+        private int _offset;
+        private int _moveVector;
+        private string _fileName;
+        Texture2D _texture;
+        public Layer(string fileName, int moveVector, ContentManager content)
+        {
+            _fileName = fileName;
+            _moveVector = moveVector;
+            _texture = content.Load<Texture2D>($"Background/{_fileName}");
+            _offset = 680 - 793 - 120;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, new Vector2(0, _offset), Color.White);
+        }
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public double getLayerPos()
+        {
+            return double.Parse(_fileName);
+        }
+    }
+}
