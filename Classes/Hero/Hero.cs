d1839a0d96aa987fc96a5a9d199e00706975716e
Nidrux <originalfroaz@gmail.com>
2022-10-19 17:08:43 +0200

    Hero update

--- Classes/Hero/Hero.cs
+++ Classes/Hero/Hero.cs
@@ -15,14 +15,15 @@ namespace Forest_of_wrath.Classes.Hero
 
         public Hero(ContentManager content)
         {
-            _heroTexture = content.Load<Texture2D>("Hero/idle");
-            _frameWidth = 150;
-            _heightOffset = 400;
+            _heroTexture = content.Load<Texture2D>("Hero/Idle");
+            _frameWidth = 184;
+            _heightOffset = 377;
             _animation = new Animation();
             _animation.AddFrame(new AnimationFrame(new Rectangle(0,0,_frameWidth,_heroTexture.Height)));
             _animation.AddFrame(new AnimationFrame(new Rectangle(_frameWidth, 0, _frameWidth, _heroTexture.Height)));
             _animation.AddFrame(new AnimationFrame(new Rectangle(_frameWidth * 2,0, _frameWidth, _heroTexture.Height)));
             _animation.AddFrame(new AnimationFrame(new Rectangle(_frameWidth * 3,0, _frameWidth, _heroTexture.Height)));
+            _animation.AddFrame(new AnimationFrame(new Rectangle(_frameWidth * 4, 0, _frameWidth, _heroTexture.Height)));
 
         }
         public void Draw(SpriteBatch spriteBatch)
---
+++ Classes/UI/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using Forest_of_wrath.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+
+namespace Forest_of_wrath.Classes.UI
+{
+    internal class Cursor : IGameObject
+    {
+        Texture2D _texture;
+        public Cursor(ContentManager content) {
+            _texture = content.Load<Texture2D>("UI/cursor");
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
